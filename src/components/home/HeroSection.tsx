bec6246d0ac6c2c0fa5efdb93f538de5d1ed0ebf
sohumgorladku <gorladku1759@students.d211.org>
2026-01-05 20:02:17 -0600

    Fix GitHub Pages config

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
-import heroImage from "@/assets/hero-conference.png";
+import heroImage from "@/assets/hero-conference.jpg";
 
 const HeroSection = () => {
   return (
