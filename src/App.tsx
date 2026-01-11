1f2c47756c623b64f2014d48ace0b742d30a0002
sohumgorladku <gorladku1759@students.d211.org>
2026-01-11 12:10:07 -0600

    Fix GitHub Pages config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -34,13 +34,12 @@ function ScrollHandler() {
   return null;
 }
 
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <ScrollHandler />
         <Routes>
           <Route path="/" element={<Index />} />
@@ -50,11 +49,10 @@ const App = () => (
           <Route path="/competitions/ethics" element={<EthicsCompetition />} />
           <Route path="/competitions/investing" element={<FinanceChallenge />} />
           <Route path="/competitions/pitch" element={<PitchCompetition />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="/events/career-panel" element={<CareerPanel />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
